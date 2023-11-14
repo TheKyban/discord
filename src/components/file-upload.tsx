60790fdf59f1251899cd3a97abb5a2f069b80cf4
aditya <aaditya1392@gmail.com>
2023-11-15 01:29:48 +0530

    image upload component working successfully

---
+++ src/components/file-upload.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { FC } from "react";
+import { UploadDropzone } from "@/lib/uploadThings";
+import "@uploadthing/react/styles.css";
+import Image from "next/image";
+import { X } from "lucide-react";
+
+interface pageProps {
+    onChange: (url?: string) => void;
+    value: string;
+    endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload: FC<pageProps> = ({ onChange, value, endpoint }) => {
+    const fileType = value?.split(".").pop();
+    if (value && fileType !== "pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image fill src={value} className="rounded-full" alt="upload" />
+                <button
+                    onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                    type="button"
+                >
+                    <X className="h-4 w-4" />
+                </button>
+            </div>
+        );
+    }
+    return (
+        <UploadDropzone
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error);
+            }}
+        />
+    );
+};
